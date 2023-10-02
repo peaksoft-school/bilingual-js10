ee46b218f9676591d241742c08812300c45e1526
Kamchybek <kamchybek@users-MacBook-Pro.local>
2023-10-02 17:38:26 +0600

    added style for button\

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ children, onClick, type }) => {
   return (
-    <button type={type} onClicik={onClick}>
+    <button style={{ color: 'green' }} type={type} onClicik={onClick}>
       {children}
     </button>
   );
