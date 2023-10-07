4335a9a9a35fdc7c632553051ce56733e00f505a
rinat2023 <riko14040000@gmail.com>
2023-10-07 19:13:58 +0600

    removed Button in appjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Button from './components/UI/Buttons/Button'
-
 function App() {
    return (
-      <>
+      <div className="App">
          <h1>Hello World</h1>
-         <Button className="toBeginButton" variant="contained">
-            register
-         </Button>
-      </>
+      </div>
    )
 }
 
