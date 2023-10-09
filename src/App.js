87d284950d488400de0658dbb26aa8fd03dd39f1
NurlanAbdullaev <nurlan2001002@gmail.com>
2023-10-10 03:05:15 +0600

    MainCounter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import styled from '@emotion/styled'
-import MainCounter from './components/MainCounter/MainCounter'
-
 function App() {
    return (
       <div>
-         {/* <h1>Hello World</h1> */}
-         <Container>
-            <h1>hello</h1>
-         </Container>
-         <MainCounter />
-         <Container>
-            <h1>hello</h1>
-         </Container>
+         <h1>Hello World</h1>
       </div>
    )
 }
 
-const Container = styled('div')`
-   width: 100%;
-   height: 700px;
-`
 export default App
