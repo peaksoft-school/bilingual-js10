e5bb65ca9f8ac20016596abc862e3bbc8c95ced2
rinat2023 <riko14040000@gmail.com>
2023-10-10 20:30:17 +0600

    updated style in OurTeam component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { UserExperience } from './components/LandingPage/UserExperience'
-import { OurTeam } from './components/LandingPage/OurTeam'
-
 function App() {
    return (
       <div>
          <h1>Bilingual js`9</h1>
-         <UserExperience />
-         <OurTeam />
       </div>
    )
 }
--- src/components/LandingPage/OurTeam.jsx
+++ src/components/LandingPage/OurTeam.jsx
@@ -2,16 +2,28 @@ import { styled } from '@mui/material'
 import React from 'react'
 import { developers } from '../../utils/helpers/Developers'
 
+const borderRadiuses = [
+   '0px 0px 0px 40px',
+   '40px 0px 40px 0px',
+   '0px 0px 40px 0px',
+   '40px 0px 0px 0px',
+   '40px 0px 40px 0px',
+   '0px 40px 0px 0px',
+   '0px 0px 40px 0px',
+]
+
 export const OurTeam = () => {
    return (
       <MainTeamContainer>
          <h1>Our Team</h1>
          <div className="ImageContainer">
-            {developers.map((developer) => (
+            {developers.map((developer, index) => (
                <div className="nameAndImageDiv" key={developer.id}>
                   <div
-                     className={`imageDiv ${developer.name}`}
-                     style={{ borderRadius: `${developer.borderRadius}` }}
+                     className="imageDiv"
+                     style={{
+                        borderRadius: borderRadiuses[index],
+                     }}
                   >
                      <img src={developer.image} alt={developer.name} />
                   </div>
@@ -23,30 +35,33 @@ export const OurTeam = () => {
       </MainTeamContainer>
    )
 }
-const MainTeamContainer = styled('div')({
-   padding: '7.5rem 6.4rem',
-   '& img': {
-      width: '11.25rem',
-      height: '11.25rem',
-   },
-   '& .imageDiv': {
-      overflow: 'hidden',
-   },
-   '& h1': {
-      textAlign: 'center',
-      marginBottom: '47px',
-      color: ' #3752B4',
-   },
-   '& b': {
-      color: ' #3A10E5',
-   },
-   '& .nameAndImageDiv': {
-      textAlign: 'center',
-   },
-   '& > .ImageContainer': {
-      display: 'flex',
-      columnGap: '8.5px',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-   },
+
+const MainTeamContainer = styled('div')(() => {
+   return {
+      padding: '7.5rem 6.4rem',
+      '& img': {
+         width: '11.25rem',
+         height: '11.25rem',
+      },
+      '& .imageDiv': {
+         overflow: 'hidden',
+      },
+      '& h1': {
+         textAlign: 'center',
+         marginBottom: '47px',
+         color: ' #3752B4',
+      },
+      '& b': {
+         color: ' #3A10E5',
+      },
+      '& .nameAndImageDiv': {
+         textAlign: 'center',
+      },
+      '& > .ImageContainer': {
+         display: 'flex',
+         columnGap: '8.5px',
+         flexWrap: 'wrap',
+         justifyContent: 'center',
+      },
+   }
 })
