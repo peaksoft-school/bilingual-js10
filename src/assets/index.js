4ed4cdb2220cc6721dee0595982955520189f661
rinat2023 <riko14040000@gmail.com>
2023-10-11 12:11:34 +0600

    fixed problems

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,4 @@
-import { ReactComponent as toBeginButtonIcon } from './icons/button.svg'
-import { ReactComponent as toBeginButtonHoverIcon } from './icons/button-hover.svg'
-
 import { ReactComponent as Zero } from './icons/Zero.svg'
 import { ReactComponent as Dolar } from './icons/Dolar.svg'
 
-export { toBeginButtonIcon, toBeginButtonHoverIcon }
 export { Zero, Dolar }
