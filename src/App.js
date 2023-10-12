765689d84d8cc7092df080f107e5bfa549e5e6f9
NurlanAbdullaev <nurlan2001002@gmail.com>
2023-10-12 14:36:22 +0600

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { FeedbackFromPeople } from './components/landingPage/FeedbackFromPeople'
-
 function App() {
    return (
       <div>
-         {/* <h1>Bilingual js`9</h1> */}
-         <FeedbackFromPeople />
+         <h1>Bilingual js`9</h1>
       </div>
    )
 }
