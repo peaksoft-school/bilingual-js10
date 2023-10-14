0b8a28af388d7fda3fc023a2fbb31aa6404802ea
rinat2023 <riko14040000@gmail.com>
2023-10-14 11:21:50 +0600

    fixed/removed component in App js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { CreateTest } from './components/test/createTest'
-
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         <CreateTest />
       </div>
    )
 }
