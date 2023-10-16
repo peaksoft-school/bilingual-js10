3199e8b99d1ccbf820eac4be9a714e7c9ef7fa0a
Dasi012 <dastandulatbekuulu@example.com>
2023-10-16 16:31:36 +0600

    added layoult

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import ListenSelect from './components/Layout/ListenSelect'
-
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         <ListenSelect />
       </div>
    )
 }
