59584946fd387be1cc54ad190d292a172d8d10aa
Dasi012 <dastandulatbekuulu@example.com>
2023-10-16 18:34:29 +0600

    fixed MultiplySelect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { MultiplySelect } from './components/UI/MultiplySelect/MultiplySelect'
 
 function App() {
    return (
       <div>
          <h1>Bilingual js-9</h1>
+         <MultiplySelect />
       </div>
    )
 }
--- src/components/UI/MultiplySelect/MultiplySelect.jsx
+++ src/components/UI/MultiplySelect/MultiplySelect.jsx
@@ -1,77 +1,30 @@
-import { styled } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import { VolumeUp } from '@mui/icons-material'
+import CheckIcon from '@mui/icons-material/Check'
+import { SelectEnglishWord } from './SelectEnglishWord'
 
-export const MultiplySelect = ({
-   text,
-   isChecked,
-   handleCheckClick,
-   CheckIcon,
-   VolumeUp,
-}) => {
+export const MultiplySelect = () => {
+   const [selectedWords, setSelectedWords] = useState([])
+
+   const handleSelectWord = (word) => {
+      if (selectedWords.includes(word)) {
+         setSelectedWords(
+            selectedWords.filter((selectedWord) => selectedWord !== word)
+         )
+      } else {
+         setSelectedWords([...selectedWords, word])
+      }
+   }
+
+   const allWords = ['Word 1', 'Word 2', 'Word 3', 'Word 4', 'Word 5', 'Word 6']
+
    return (
-      <Container>
-         <div className={`ContainerMultiply ${isChecked ? 'checked' : ''}`}>
-            <div className="textCon">
-               {VolumeUp}
-               <p>{text}</p>
-            </div>
-            <div className="InputCheckBox">
-               <button
-                  className={`IconValue ${isChecked ? 'checked' : ''}`}
-                  onClick={handleCheckClick}
-               >
-                  {CheckIcon}
-               </button>
-            </div>
-         </div>
-      </Container>
+      <SelectEnglishWord
+         words={allWords}
+         selectedWords={selectedWords}
+         handleSelectWord={handleSelectWord}
+         VolumeUp={<VolumeUp />}
+         CheckIcon={<CheckIcon />}
+      />
    )
 }
-
-const Container = styled('div')(() => ({
-   '.ContainerMultiply': {
-      display: 'flex',
-      gap: '2rem',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      border: '1.5px solid #D4D0D0',
-      borderRadius: '0.5rem',
-      width: '12.3rem',
-      height: '2.6rem',
-      paddingLeft: '10px',
-      color: '#4C4859',
-      lineHeight: '1rem',
-   },
-   ' .textCon': {
-      display: 'flex',
-      justifyContent: 'spase-between',
-      gap: '0.7rem',
-      alignItems: 'center',
-   },
-   ' .InputCheckBox': {
-      display: 'flex',
-      width: '3rem',
-      height: '2.6rem',
-      justifyContent: 'center',
-      alignItems: 'center',
-   },
-   ' .checked': {
-      width: '12.4rem',
-      color: '#4C4859',
-      border: ' 2px solid #3A10E5',
-   },
-   ' .IconValue': {
-      width: '3.3rem',
-      height: '2.6rem',
-      border: '1.5px solid #D4D0D0',
-      borderRadius: '0rem 0.5rem 0.5rem 0rem',
-      borderRight: 'none',
-      color: '#D4D0D0',
-      background: 'white',
-   },
-   ' .IconValue.checked': {
-      color: 'white',
-      background: '#3A10E5',
-      border: ' 1.5px solid #3A10E5',
-   },
-}))
