bba3b55bc4ac12938d61e404824e4d19210cd186
NurlanAbdullaev <nurlan2001002@gmail.com>
2023-10-17 17:37:39 +0600

    Notify

--- src/components/UI/Notifay.jsx
+++ src/components/UI/Notifay.jsx
@@ -1,71 +1,42 @@
-import { ToastContainer, toast } from 'react-toastify'
+import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { styled } from '@mui/material'
 import { StickRed } from '../../assets'
 
-const Notify = (messageVariant, messages, text) => {
-   const showLoadingToast = () => {
-      return toast.promise(
-         new Promise((resolve, reject) => {
-            setTimeout(() => {
-               if (messageVariant === 'success') {
-                  resolve()
-               } else {
-                  reject()
-               }
-            }, 1000)
-         }),
-         {
-            pending: 'Promise is pending...',
-            success: {
-               render() {
-                  return (
-                     <SuccessToast>
-                        <h4>{messages}</h4>
-                        <p>{text}</p>
-                     </SuccessToast>
-                  )
-               },
-               style: {
-                  backgroundColor: '#e9fbe7',
-                  height: '6rem',
-               },
-            },
-            error: {
-               render() {
-                  return (
-                     <ErrorToast>
-                        <h4>{messages}</h4>
-                        <p>{text}</p>
-                     </ErrorToast>
-                  )
-               },
-               style: {
-                  backgroundColor: '#fff1f1',
-                  height: '6rem',
-               },
-               icon: <StickRed />,
-            },
-         }
-      )
-   }
-
-   showLoadingToast()
-
-   return (
-      <ToastContainer
-         position="top-right"
-         autoClose={2000}
-         hideProgressBar={false}
-         newestOnTop={false}
-         closeOnClick
-         rtl={false}
-         pauseOnFocusLoss
-         draggable
-         pauseOnHover
-         theme="light"
-      />
-   )
+const Notify = (messages, promise) => {
+   const { sucessTitle, successMessage, errorTitle } = messages
+   return toast.promise(promise, {
+      pending: 'Promise is pending...',
+      success: {
+         render() {
+            return (
+               <SuccessToast>
+                  <h4>{sucessTitle}</h4>
+                  <p>{successMessage}</p>
+               </SuccessToast>
+            )
+         },
+         style: {
+            backgroundColor: '#e9fbe7',
+            height: '6rem',
+         },
+      },
+      error: {
+         render({ data }) {
+            return (
+               <ErrorToast>
+                  <h4>{errorTitle}</h4>
+                  <p>{data.message}</p>
+               </ErrorToast>
+            )
+         },
+         style: {
+            backgroundColor: '#fff1f1',
+            height: '6rem',
+         },
+         icon: <StickRed />,
+      },
+   })
 }
 export default Notify
 const SuccessToast = styled('div')`
