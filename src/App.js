f29317e10483c4512176fc6ba21292dca722d343
Turdugul <okonbaevaturdugul@gmail.com>
2023-10-19 12:49:16 +0100

    partners slider done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import SliderPartner from './components/LandingPage/SliderPartner'
 
 function App() {
    return (
       <div>
-         <h1>Bilingual js-9</h1>
-         <SliderPartner />
+         <h1>hello</h1>
       </div>
    )
 }
--- src/utils/helpers/PartnersIcons.jsx
+++ src/utils/helpers/PartnersIcons.jsx
@@ -10,15 +10,12 @@
 
 import adama from '../../assets/icons/adama.svg'
 import decalb from '../../assets/icons/decalb.svg'
-import don from '../../assets/icons/donit.svg'
 import basf from '../../assets/icons/basf.svg'
 import lidea from '../../assets/icons/lidea.svg'
 
 export const partnersIcons = [
-   { id: 1, icon: don },
-   { id: 2, icon: basf },
-   { id: 3, icon: lidea },
-   { id: 4, icon: adama },
-   { id: 5, icon: decalb },
-   { id: 6, icon: basf },
+   { id: 1, icon: basf },
+   { id: 2, icon: lidea },
+   { id: 3, icon: adama },
+   { id: 4, icon: decalb },
 ]
