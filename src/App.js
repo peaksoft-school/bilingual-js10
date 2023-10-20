7f5029a335e7292ac437ad3e4134f0c8aee8bd91
GULAIYM EDILBAEVA <gulaiymhope@gmail.com>
2023-10-20 12:13:01 +0600

    deletions not needed in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-// import { useState } from 'react'
-// import SignIn from './components/authForm/SignIn'
-// import SignUp from './components/authForm/SignUp'
-
-// Для SIGNIN и SIGNUP.ИШТЕТИП КОРГОНГО
 function App() {
-   // const [isSignIn, setIsSignIn] = useState(true)
-   // const toggleSignInSignUp = () => {
-   //    setIsSignIn(!isSignIn)
-   // }
    return (
       <div>
-         {/* {isSignIn ? (
-            <SignIn toggleSignUp={toggleSignInSignUp} />
-         ) : (
-            <SignUp toggleSignIn={toggleSignInSignUp} />
-         )} */}
          <h1>Bilingual js-10</h1>
       </div>
    )
