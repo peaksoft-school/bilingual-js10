8a80964a9fa7f5be2961d36228c3fbc4c0c0b86d
rinat2023 <riko14040000@gmail.com>
2023-10-23 13:07:02 +0600

    fixed removed draganddop in app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { DragAndDrop } from './components/clientTest/DragAndDrop/DragAndDrop'
-
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         <DragAndDrop />
       </div>
    )
 }
