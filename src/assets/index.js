f0440d71ef16b04890382d8063c506b9827cfa46
NurlanAbdullaev <nurlan2001002@gmail.com>
2023-10-24 18:11:47 +0600

    index.js conflickt

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,6 +21,7 @@ import { ReactComponent as StickBlue } from './icons/stickBlue.svg'
 import { ReactComponent as StickPink } from './icons/stickPink.svg'
 import { ReactComponent as RoundLeftBlue } from './icons/RoundLeftBlue.svg'
 import { ReactComponent as RoundRightBlue } from './icons/RoundRightBlue.svg'
+
 import { ReactComponent as TrashCan } from './icons/trashCan.svg'
 import { ReactComponent as Check } from './icons/check.svg'
 import { ReactComponent as Eyes } from './icons/eyes.svg'
