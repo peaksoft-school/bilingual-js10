48f69fd63e0025c4bc1de7d4f53bf4b05834dde8
NurlanAbdullaev <nurlan2001002@gmail.com>
2023-10-26 13:04:29 +0600

    test

--- src/utils/TableKopi.jsx
+++ src/utils/TableKopi.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table } from './components/UI/Table'
+import { Table } from './components/components/table/Table'
 import { Check, Eye, TrashCan } from './assets'
 import { styled } from '@mui/material'
 
