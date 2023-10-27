1179eb962ebc536ae521ac351918789c11079d01
Dasi012 <dastandulatbekuulu@example.com>
2023-10-28 00:56:29 +0600

    added components SelectBestModal and SelectBestTitle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { SelectBestTitle } from './layout/SelectBestTitle'
-
 function App() {
    return (
       <div>
-         {/* <h1>Bilingual js-10</h1> */}
-         <SelectBestTitle />
+         <h1>Bilingual js-10</h1>
       </div>
    )
 }
