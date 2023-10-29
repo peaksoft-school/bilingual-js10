4450f2ea794bdd1a3e85804c8cc0bf5e579a4d8f
Dasi012 <dastandulatbekuulu@example.com>
2023-10-29 20:26:49 +0600

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-// import { SelectBestTitle } from './components/adminTest/SelectTheBestTitle/SelectBestTitle'
-import CustomFormCreateTest from './components/adminTest/customFormCreateTest/CustomFormCreateTest'
-
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         {/* <SelectBestTitle /> */}
-         <CustomFormCreateTest />
       </div>
    )
 }
