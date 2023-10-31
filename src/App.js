18dd1da3110908754cce889a52a409f865de9623
rinat2023 <riko14040000@gmail.com>
2023-10-31 12:36:39 +0600

    fixed problems and conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { TestList } from './components/test/TestList'
 
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         <TestList />
       </div>
    )
 }
