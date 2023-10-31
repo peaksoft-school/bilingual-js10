fc92cad4d9a31df221dbdc91b3d1ec0964f512e1
rinat2023 <riko14040000@gmail.com>
2023-10-31 19:30:37 +0600

    added recording

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Recording } from './components/clientTest/Recording'
+import React from 'react'
+import Example from './components/clientTest/Recording'
 
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         <Recording />
+         <Example />
       </div>
    )
 }
+
 export default App
--- src/components/clientTest/Recording.jsx
+++ src/components/clientTest/Recording.jsx
@@ -45,37 +45,45 @@
 // })
 
 import React, { useState } from 'react'
-import ReactMic from 'react-mic'
+import { ReactMic } from 'react-mic'
 
-export const Recording = () => {
-   const [isRecording, setIsRecording] = useState(false)
+function Example() {
+   const [record, setRecord] = useState(false)
 
    const startRecording = () => {
-      setIsRecording(true)
+      setRecord(true)
    }
 
    const stopRecording = () => {
-      setIsRecording(false)
+      setRecord(false)
    }
 
-   // const onData = (recordedData) => {
-   //    // Обработка данных, записанных в реальном времени.
-   // }
+   const onData = (recordedBlob) => {
+      console.log('chunk of real-time data is: ', recordedBlob)
+   }
 
-   // const onStop = (recordedData) => {
-   //    // Обработка данных после остановки записи.
-   // }
+   const onStop = (recordedBlob) => {
+      console.log('recordedBlob is: ', recordedBlob)
+   }
 
    return (
       <div>
-         <button onClick={startRecording}>Начать запись</button>
-         <button onClick={stopRecording}>Остановить запись</button>
          <ReactMic
-            record={isRecording}
+            record={record}
             className="sound-wave"
             onStop={onStop}
             onData={onData}
+            strokeColor="#ffffff"
+            backgroundColor="#000000"
          />
+         <button onClick={startRecording} type="button">
+            Start
+         </button>
+         <button onClick={stopRecording} type="button">
+            Stop
+         </button>
       </div>
    )
 }
+
+export default Example
