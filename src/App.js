cc5bc127d560ca1b85d388f5fdd1d51b87bad7e6
rinat2023 <riko14040000@gmail.com>
2023-10-31 21:21:00 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { HighlightTheAnswer } from './components/test/HighlightTheAnswer'
 
 function App() {
    return (
       <div>
          <h1>Bilingual js-10</h1>
-         <HighlightTheAnswer />
       </div>
    )
 }
