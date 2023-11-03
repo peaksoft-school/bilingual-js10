260bc567bb4987160e7b98dec52b5f79533926ea
rinat2023 <riko14040000@gmail.com>
2023-11-03 15:36:21 +0600

    fixed/added testListArr

--- src/components/clientTest/testList/TestList.jsx
+++ src/components/clientTest/testList/TestList.jsx
@@ -2,16 +2,41 @@ import React from 'react'
 import { styled } from '@mui/material'
 import { Background } from '../../../layout/Background'
 import { testListArr } from '../../../utils/helpers/testListArr'
-import { BilingualLogo } from '../../../assets'
+import { BilingualLogo, TestListIcon } from '../../../assets'
+import Button from '../../UI/Buttons/Button'
 
-const TestList = () => {
+const TestList = ({ onClickTryTest }) => {
    return (
       <PurpleBackground>
-         <Background marginTop="80px">
-            {testListArr[0].map((test) => {
-               return test.testList
-            })}
-         </Background>
+         {testListArr.map((test) => {
+            return (
+               <Background
+                  marginTop={testListArr.length > 2 ? '20px' : '80px'}
+                  key={test.id}
+               >
+                  <ListContainerStyle>
+                     <div className="mainContainer">
+                        <TestListIcon />
+                        <div className="description">
+                           <p>{test.minutes} MINUTES</p>
+                           <p>{test.title}</p>
+                           <p>Train as much as you like.</p>
+                        </div>
+                     </div>
+                     <Button
+                        onClick={onClickTryTest}
+                        variant="outlined"
+                        hoverStyle="#3A10E5"
+                     >
+                        try test
+                     </Button>
+                  </ListContainerStyle>
+               </Background>
+            )
+         })}
+         <br />
+         <br />
+         <br />
          <FooterStyle>
             <BilingualLogo />
             <p>© 2022 Peaksoft</p>
@@ -24,9 +49,10 @@ export default TestList
 
 const PurpleBackground = styled('div')({
    backgroundColor: '#D7E1F8',
-   position: 'fixed',
    height: '100vh',
    width: '100vw',
+   display: 'flex',
+   flexDirection: 'column',
 })
 
 const FooterStyle = styled('div')({
@@ -41,5 +67,39 @@ const FooterStyle = styled('div')({
    columnGap: '12px',
    p: {
       color: '#98A2B3',
+   },
+})
+
+const ListContainerStyle = styled('div')({
+   display: 'flex',
+   alignItems: 'end',
+   columnGap: '16rem',
+   '.mainContainer': {
+      display: 'flex',
+      alignItems: 'center',
+      columnGap: '45px',
+      '.description': {
+         display: 'flex',
+         flexDirection: 'column',
+         rowGap: '15px',
+         '& :first-child': {
+            color: '#3A10E5',
+            fontStyle: 'normal',
+            fontSize: '15px',
+            fontWeight: '900',
+         },
+         '& :nth-child(2)': {
+            color: '#4C4859',
+            fontStyle: 'normal',
+            fontSize: '26px',
+            fontWeight: '500',
+         },
+         '& :nth-child(3)': {
+            color: '#4C4859',
+            fontStyle: 'normal',
+            fontSize: '16px',
+            fontWeight: '400',
+         },
+      },
    },
 })
--- src/utils/helpers/testListArr.js
+++ src/utils/helpers/testListArr.js
@@ -1,3 +1,26 @@
-import { TestListPage } from '../../components/test/TestListPage'
-
-export const testListArr = [[{ testList: <TestListPage /> }]]
+export const testListArr = [
+   {
+      minutes: 435,
+      title: 'English beginner test',
+   },
+   {
+      minutes: 1325,
+      title: 'English advanced test',
+   },
+   {
+      minutes: 43457,
+      title: 'English advanced test',
+   },
+   {
+      minutes: 1,
+      title: 'English advanced test',
+   },
+   {
+      minutes: 199,
+      title: 'English advanced test',
+   },
+   {
+      minutes: 5315,
+      title: 'English advanced test',
+   },
+]
