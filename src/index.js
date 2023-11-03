9ad2d6dd7645f69744196797cd1fde84cbcdd5f6
GULAIYM EDILBAEVA <gulaiymhope@gmail.com>
2023-11-03 12:51:13 +0600

    save

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@emotion/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
@@ -9,18 +10,21 @@ import { theme } from './assets/theme/globalTheme'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import App from './App'
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
-      <ThemeProvider theme={theme}>
+      <Provider store={store}>
          <BrowserRouter>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-               <ToastContainer />
-               <App />
-            </LocalizationProvider>
+            <ThemeProvider theme={theme}>
+               <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <ToastContainer />
+                  <App />
+               </LocalizationProvider>
+            </ThemeProvider>
          </BrowserRouter>
-      </ThemeProvider>
+      </Provider>
    </React.StrictMode>
 )
 reportWebVitals()
