f43f3a0e18726f7c498afa47414577d64f2983e0
Turdugul <okonbaevaturdugul@gmail.com>
2023-11-21 07:01:52 +0000

    added approutes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { USER_KEY } from './utils/constants/constants'
-// import { AppRoutes } from './routes/AppRoutes'
+import { AppRoutes } from './routes/AppRoutes'
 import { authActions } from './store/auth/authSlice'
-import CustomFormCreateTest from './components/adminTest/customFormCreateTest/CustomFormCreateTest'
 
 function App() {
    const dispatch = useDispatch()
@@ -28,7 +27,7 @@ function App() {
    return (
       <div>
          {/* <h1>Billingual-10</h1>{' '} */}
-         <CustomFormCreateTest />
+         <AppRoutes />
       </div>
    )
 }
