c934d721a25802d447b920c4bef3c74730b6bed9
rinat2023 <riko14040000@gmail.com>
2023-11-23 11:36:53 +0600

    save

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { USER_KEY } from './utils/constants/constants'
-// import { AppRoutes } from './routes/AppRoutes'
+import { AppRoutes } from './routes/AppRoutes'
 import { authActions } from './store/auth/authSlice'
-import LandingPage from './pages/LandingPage'
+// import LandingPage from './pages/LandingPage'
 
 function App() {
    const dispatch = useDispatch()
