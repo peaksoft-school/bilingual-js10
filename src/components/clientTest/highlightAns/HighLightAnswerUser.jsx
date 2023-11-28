ab85ee48da19d5d4db56a1db31c4d9fd87d6a49b
Turdugul <okonbaevaturdugul@gmail.com>
2023-11-28 14:37:27 +0000

    component done

--- src/components/clientTest/highlightAns/HighLightAnswerUser.jsx
+++ src/components/clientTest/highlightAns/HighLightAnswerUser.jsx
@@ -8,7 +8,19 @@ import Button from '../../UI/Buttons/Button'
 import TextArea from '../../UI/textarea/TextArea'
 import { addTest } from '../../../store/userTest/global-test-slice'
 
-const HighLightAnswerUser = ({ text }) => {
+const passage = `Sed ut perspiciatis unde omnis iste natus error sit
+                           voluptatem accusantium doloremque laudantium, totam
+                           rem aperiam, eaque ipsa quae ab illo inventore
+                           veritatis et quasi architecto beatae vitae dicta sunt
+                           explicabo. Nemo enim ipsam voluptatem quia voluptas
+                           sit aspernatur aut odit aut fugit, sed quia
+                           consequuntur magni dolores eos qui ratione voluptatem
+                           sequi nesciunt. Neque porro quisquam est, qui dolorem
+                           ipsum quia dolor sit amet, consectetur, adipisci
+                           velit, sed quia non numquam eius modi tempora
+                           incidunt ut labore et dolore magnam aliquam quaerat
+                           voluptatem`
+const HighLightAnswerUser = () => {
    const [answerValue, setAnswerValue] = useState('')
    const dispatch = useDispatch()
    const duration = 40
@@ -46,8 +58,7 @@ const HighLightAnswerUser = ({ text }) => {
                               setAnswerValue(window.getSelection().toString())
                            }
                         >
-                           text comes with props=== Sed ut perspiciatis unde
-                           omnis iste natus error sit voluptatem {text}
+                           {passage}
                         </Pstyle>
                      </TextBox>
                   </PassageBlock>
