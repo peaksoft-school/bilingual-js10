077163c68afd24e3355ee7c7c35628f19857e3a8
Dasi012 <dastandulatbekuulu@gmail.com>
2023-11-28 11:14:25 +0600

    added components SelectTheBestTitle

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { USER_KEY } from './utils/constants/constants'
-import { AppRoutes } from './routes/AppRoutes'
+// import { AppRoutes } from './routes/AppRoutes'
 import { authActions } from './store/auth/authSlice'
+import { SelectTheBestTitle } from './components/clientTest/SelectTheBest/SelectTheBestTitle'
+// import { InputRadio } from './components/UI/InputRadio'
 // import LandingPage from './pages/LandingPage'
 
 function App() {
@@ -28,9 +30,10 @@ function App() {
 
    return (
       <div>
-         <AppRoutes />
+         {/* <AppRoutes /> */}
          {/* <LandingPage /> */}
          {/* <CustomFormCreateTest /> */}
+         <SelectTheBestTitle />
       </div>
    )
 }
