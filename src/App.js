67612dca4a870afbe3280b3da3734316829cfa7a
Dasi012 <dastandulatbekuulu@gmail.com>
2023-12-03 10:18:55 +0600

    fixed App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { USER_KEY } from './utils/constants/constants'
 import { authActions } from './store/auth/authSlice'
-import ListenAndSelectEnglishWord from './components/adminCheckingUserTest/ListenAndSelectEnglishWord'
-// import { AppRoutes } from './routes/AppRoutes'
+import { AppRoutes } from './routes/AppRoutes'
 
 function App() {
    const dispatch = useDispatch()
@@ -28,8 +27,7 @@ function App() {
 
    return (
       <div>
-         {/* <AppRoutes /> */}
-         <ListenAndSelectEnglishWord />
+         <AppRoutes />
       </div>
    )
 }
